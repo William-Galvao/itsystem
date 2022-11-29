93894d9e4876845e57f205ba88d443cae04842d3
Willian Galvão <willian.galvao@katrium.local>
2022-11-29 17:12:16 -0300

    Working on form validations

--- src/views/CadastrarCliente.vue
+++ src/views/CadastrarCliente.vue
@@ -14,6 +14,9 @@ const cargo = ref("")
 const telefone = ref("")
 const celular = ref("")
 const email = ref("")
+const erro = ref(null)
+const loading = ref(false)
+const success = ref(false)
 
 const cliente = computed(() => ({
     Ativo: ativo.value,
@@ -28,8 +31,34 @@ const cliente = computed(() => ({
 }))
 
 async function postNovoCliente() {
+    loading.value = true;
+    if (cliente.value.CNPJ.length != 14) {
+        erro.value = "O CNPJ deve conter obrigatoriamente 14 digitos."
+        loading.value = false;
+        setTimeout(() => erro.value = null, 5000)
+        return
+    }
+    if (/^\d+$/.test(cliente.value)) {
+        erro.value = "O CNPJ deve conter apenas numeros."
+        loading.value = false;
+        setTimeout(() => erro.value = null, 5000)
+        return
+    }
     const docRef = await addDoc(collection(db, "clientes"), cliente.value);
-    console.log("Document written with ID: ", docRef.id);
+    loading.value = false;
+    success.value = true;
+    
+    ativo.value = true
+    cnpj.value = ""
+    razaoSocial.value = ""
+    endereco.value = ""
+    nomeContato.value = ""
+    cargo.value = ""
+    telefone.value = ""
+    celular.value = ""
+    email.value = ""
+
+    setTimeout(() => success.value = false, 5000);
 
 }
 
@@ -93,9 +122,40 @@ async function postNovoCliente() {
                 </a>
             </button>
             <button @click="postNovoCliente" class="bg-green-600 text-white px-8 py-1 rounded-lg hover:bg-green-800">
-                <fa icon="floppy-disk" class="mr-1" />
-                Salvar
+                <span v-if="loading">
+                    <svg aria-hidden="true"
+                        class="mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                        viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor" />
+                        <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill" />
+                    </svg>
+                </span>
+                <span v-if="!loading">
+                    <fa icon="floppy-disk" class="mr-1" />
+                    Salvar
+                </span>
             </button>
+            <div v-if="erro"
+                class="animate__animated animate__bounceInDown absolute top-5 w-full flex justify-center items-center">
+                <div
+                    class="text-white bg-red-600 rounded-lg flex justify-center items-center w-full py-4 gap-x-4 px-2 md:mx-2">
+                    <fa class="text-xl" icon="circle-exclamation" />
+                    <span class="font-semibold">{{ erro }}</span>
+                </div>
+            </div>
+
+            <div v-if="success"
+                class="animate__animated animate__bounceInDown absolute top-5 w-full flex justify-center items-center">
+                <div
+                    class="text-white bg-green-600 rounded-lg flex justify-center items-center w-full py-4 gap-x-4 px-2 md:mx-2">
+                    <fa class="text-xl" icon="check" />
+                    <span class="font-semibold">A nova empresa foi cadastrada com sucesso.</span>
+                </div>
+            </div>
         </div>
     </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,8 @@ import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 const selectedMenu = ref('')
 const clientes = ref([])
 const loading = ref(false)
+const popup = ref(false)
+const cliente = ref({})
 
 async function getAllClientes() {
     let fetchedClientes = []
@@ -29,7 +31,7 @@ async function getAllClientes() {
         fetchedClientes.push(cliente)
         clientes.value = fetchedClientes;
         loading.value = false;
-        
+
     });
 }
 
@@ -48,10 +50,26 @@ async function getCliente(id) {
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
+        popup.value = true
+        cliente.value = {
+            Ativo: docSnap.data().Ativo,
+            CNPJ: docSnap.data().CNPJ,
+            RazaoSocial: docSnap.data().RazaoSocial,
+            Endereco: docSnap.data().Endereco,
+            NomeContato: docSnap.data().NomeContato,
+            Cargo: docSnap.data().Cargo,
+            Telefone: docSnap.data().Telefone,
+            Celular: docSnap.data().Celular,
+            Email: docSnap.data().Email
+        }
         console.log("Document data:", docSnap.data());
     } else {
         console.log("No such document!");
     }
+}
+
+function closePopup() {
+    popup.value = false;
 }
 
 
@@ -115,7 +133,8 @@ async function getCliente(id) {
                         <td>{{ cliente.CNPJ }}</td>
                         <td>
                             <div class="flex content-center justify-evenly">
-                                <button @click="getCliente(cliente.id)" class="text-white bg-gray-500 px-3 py-2 rounded-lg hover:bg-gray-600">
+                                <button @click="getCliente(cliente.id)"
+                                    class="text-white bg-gray-500 px-3 py-2 rounded-lg hover:bg-gray-600">
                                     <fa icon="magnifying-glass" />
                                 </button>
                                 <button class="text-white bg-gray-500 px-3 py-2 rounded-lg hover:bg-gray-600">
@@ -131,13 +150,37 @@ async function getCliente(id) {
                 </tbody>
             </table>
 
+
+
             <a v-if="selectedMenu == 'funcionarios'" href="/cadastrarfuncionario"
                 class="self-center w-1/6 text-center px-4 py-2 bg-indigo-500 text-white font-semibold rounded-lg hover:bg-indigo-800">
                 <span class="hidden md:block">Novo Funcionario</span>
                 <fa class="md:hidden" icon="plus" />
             </a>
         </main>
-
+        <div v-if="popup"
+            class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex justify-center items-center">
+            <div class="py-4 md:py-0 mx-1 md:mx-0 bg-white md:w-1/2 md:h-1/2 flex flex-col justify-center px-4 gap-y-2">
+                <div class="border-b-2 border-gray-800 pb-2">
+                    <p><span class="font-semibold">{{cliente.Ativo? "Ativo" : "Inativo"}}</span></p>
+                    <p><span class="text-sm font-semibold">Razao Social:</span> {{ cliente.RazaoSocial }}</p>
+                    <p><span class="text-sm font-semibold">CNPJ:</span> {{ cliente.CNPJ }}</p>
+                    <p><span class="text-sm font-semibold">Endereco:</span> {{ cliente.Endereco }}</p>
+                </div>
+                <div class="flex flex-col justify-start">
+                    <p><span class="text-sm font-semibold">Contato:</span> {{ cliente.NomeContato }}</p>
+                    <p><span class="text-sm font-semibold">Cargo:</span> {{ cliente.Cargo }}</p>
+                    <p><span class="text-sm font-semibold">Telefone:</span> {{ cliente.Telefone }}</p>
+                    <p><span class="text-sm font-semibold">Celular:</span> {{ cliente.Celular }}</p>
+                    <p><span class="text-sm font-semibold">E-mail:</span> {{ cliente.Email }}</p>
+                </div>
+                <button @click="closePopup"
+                    class="self-center mt-2 w-1/5 px-3 py-2 md:px-0 md:py-1 bg-red-600 text-white rounded-lg hover:bg-red-800 flex items-center justify-center gap-x-2">
+                    <fa icon="rotate-left" class="mr-1" />
+                    <span class="hidden md:block">Voltar</span>
+                </button>
+            </div>
+        </div>
     </div>
 
 </template>
